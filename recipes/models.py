440deeec30daeb4882dfdce3c3026f1473423cee
darthjee <darthjee@gmail.com>
2019-02-11 19:15:38 +0100

    Count recipes

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Recipe(Base):
+class Recipe(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 
 from django.http import HttpResponse
+from recipes.models import Recipe
 
 def index(request):
-  return HttpResponse("Recipes Index")
+    return HttpResponse("Recipes Index: %d" % Recipe.objects.count())
