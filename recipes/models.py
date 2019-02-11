e61ff7e7693da7a84c2441255506976a3dcc3db0
darthjee <darthjee@gmail.com>
2019-02-11 19:10:28 +0100

    Add to string

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
-class Recipe(models.Model):
-  name = models.CharField(max_length=200)
+class Recipe(Base):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
